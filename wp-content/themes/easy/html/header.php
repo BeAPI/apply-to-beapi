70c9d5ad0f7ba1a8cf00cae2cf5f8ece69b1e665
Apply to BeAPI <rh@beapi.fr>
2017-09-28 16:29:26 +0200

    project push from local

---
+++ wp-content/themes/easy/html/header.php
@@ -0,0 +1,36 @@
+<head>
+	<!-- title -->
+	<meta charset="utf-8">
+	<title>Paul - Test BeAPI</title>
+
+	<!-- description -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1">
+	<meta name="author" content="Pixelshow">
+	<meta name="description" content="Personal Portfolio Template">
+
+	<!-- favicon -->
+	<link rel="shortcut icon" href="images/favicon.png" type="image/png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114x114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72x72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/apple-touch-icon-57x57-precomposed.png">
+
+	<!-- fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+
+	<!-- styles -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+	 <!-- Javascript libraries -->
+	<script type="text/javascript" src="js/modernizr.js"></script>
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+
+	<!--[if IE]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+<?php wp_head(); ?>
+</head>
--- wp-content/themes/easy/php-exercice/answers.php
+++ wp-content/themes/easy/php-exercice/answers.php
@@ -1,1 +1,87 @@
 <?php
+
+	//DISPLAY
+
+	$a="Hello";
+	$b="Hello 5 !";
+	$c="Hello \'";
+	$d=false;
+
+	echo $a;
+	echo $b;
+	echo $c;
+
+	if ($a=="Hello")
+	{
+		$d=true;
+	}
+	else $d=false;
+
+	$a="Hello", $b="Hello 5 !", $c="Hello \'";
+
+	//LOOP
+
+	$i=0;
+
+	while ($i<= 10) {
+		echo $i;
+		$i++;
+	}
+
+	for ($i=0; $i<=10; $i++) {
+		echo $i;
+	}
+
+	function affichageNbrMax($max)
+	{
+		while ($max<= 10) {
+			echo $max;
+			$max++;
+		}
+	}
+
+	affichageNbrMax(20);
+
+	//ARRAY
+
+	array $tab(5, 12, 10, 21, 2, 6, 8, 20, 14, 9, 12);
+
+	//DISPLAY
+	for ($i=0; $i<=sizeof($tableau); $i++) {
+		echo $tab[$i];
+	}
+
+	//MOYENNE
+	$somme=0;
+	$moyenne=0;
+	for ($i=0; $i<=sizeof($tableau); $i++) {
+		$somme=$somme+$tab[$i];
+	}
+	$moyenne=$somme/sizeof($tableau);
+	echo $moyenne;
+
+	//NombreAPlusDe10
+	$plusDeDix=0;
+	for ($i=0; $i<=sizeof($tableau); $i++) {
+		if ($tab[$i]>=10) {
+			$plusDeDix++;
+		}
+	}
+
+
+	//VerifSi20Present
+	$present=false;
+	for ($i=0; $i<=sizeof($tableau); $i++) {
+		if ($tab[$i]=20) {
+			$present=true;
+		}
+	}
+
+	//PlusForteValeur
+	$plusForteValeur=0;
+	for ($i=0; $i<=sizeof($tableau); $i++) {
+		if ($tab[$i]>$plusForteValeur) {
+			$plusForteValeur=$tab[$i];
+		}
+	}
+?>
